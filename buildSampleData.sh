89f2e1f2e09e4bd701b386c0672ac0d5e6f646e4
Franco Nogarin <spydmobile@gmail.com>
2024-11-28 09:09:32 -0700

    add manual shell script to setup sample data

---
+++ buildSampleData.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Load configuration
+if [ -f ./configVars.sh ]; then
+  echo "Loading configVars.sh"
+  . ./configVars.sh || { echo "Could not load configVars.sh"; exit 1; }
+else
+  echo "Could not find ./configVars.sh"; exit 1;
+fi
+
+# make the destination path by combining WISE_EXTERNAL_DATA_FOLDER + WISE_DATASET_FOLDER
+
+# Ensure the script exits on any error
+set -e
+
+# Combine the environment variables into a destination path
+DESTINATION_PATH="${WISE_EXTERNAL_DATA_FOLDER}/${WISE_DATASET_FOLDER}/"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$DESTINATION_PATH"
+
+# Print the destination path for debugging
+echo "Destination path: $DESTINATION_PATH"
+
+
+
+
+cat ./sample_data/wise_demo_data_part-* > ./sample_data/wise_demo_data.zip
+unzip ./sample_data/wise_demo_data.zip -d $DESTINATION_PATH
